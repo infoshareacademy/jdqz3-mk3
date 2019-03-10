d5783e73272fe79c1e6b46fa7205d0a4ae5a742e
Krzysztof Bednarski <bednarskik@gmail.com>
2019-03-10 12:42:11 +0100

    Add elements classes

---
+++ jdqz3-projekt-mk3/src/main/java/elements/Select.java
@@ -0,0 +1,30 @@
+package elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Select {
+    private int timeout = 10;
+    private WebElement element;
+    private WebDriver driver;
+    private org.openqa.selenium.support.ui.Select select;
+
+    public Select(WebDriver driver, By by) {
+        this.driver = driver;
+        WebDriverWait wait = new WebDriverWait(this.driver, timeout);
+        element = wait.until(ExpectedConditions.elementToBeClickable((by)));
+        select = new org.openqa.selenium.support.ui.Select(element);
+    }
+
+    public Select chooseOption(String text) {
+        this.select.selectByVisibleText(text);
+        return this;
+    }
+
+    public String readSelectedOption() {
+        return this.select.getFirstSelectedOption().getText();
+    }
+}
