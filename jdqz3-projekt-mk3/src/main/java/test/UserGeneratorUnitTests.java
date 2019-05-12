761fe64a5ab43d7abea5d034bf56f3cb177eaa18
Krzysztof Bednarski <bednarskik@gmail.com>
2019-05-12 13:32:34 +0200

    Add more unit tests for UserGenerator

--- jdqz3-projekt-mk3/src/main/java/test/UserGeneratorUnitTests.java
+++ jdqz3-projekt-mk3/src/main/java/test/UserGeneratorUnitTests.java
@@ -2,27 +2,15 @@ package test;
 
 import generators.UserGenerator;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import static java.lang.Character.isUpperCase;
 
 public class UserGeneratorUnitTests {
-//    private UserGenerator userGenerator;
-//
-//    @Before
-//    public void setUp() {
-//        userGenerator = new UserGenerator();
-//    }
 
     @Test
     public void isUserFirstNameLength9() {
         Assert.assertEquals(9, UserGenerator.firstNameGenerator().length());
-    }
-
-    @Test
-    public void isUserLastNameLength15() {
-        Assert.assertEquals(15, UserGenerator.lastNameGenerator().length());
     }
 
     @Test
@@ -32,9 +20,37 @@ public class UserGeneratorUnitTests {
     }
 
     @Test
+    public void isUserLastNameLength15() {
+        Assert.assertEquals(15, UserGenerator.lastNameGenerator().length());
+    }
+
+    @Test
     public void isFirstLetterInLastNameCapital() {
         char firstLetter = UserGenerator.lastNameGenerator().charAt(0);
         Assert.assertTrue(isUpperCase(firstLetter));
+    }
+
+    @Test
+    public void isBillingCompanyLength10() {
+        Assert.assertEquals(10, UserGenerator.billingCompanyGenerator().length());
+    }
+
+    @Test
+    public void isFirstLetterInBillingCompanyCapital() {
+        char firstLetter = UserGenerator.billingCompanyGenerator().charAt(0);
+        Assert.assertTrue(isUpperCase(firstLetter));
+    }
+
+    @Test
+    public void isAtSignInEmailAddress() {
+        String atSign = "@";
+        Assert.assertTrue(UserGenerator.emailGenerator().contains(atSign));
+    }
+
+    @Test
+    public void isPrefixInPnoneNumber() {
+        String prefix = "+48";
+        Assert.assertTrue(UserGenerator.phoneNumberGenerator().contains(prefix));
     }
 
     @Test
@@ -42,5 +58,10 @@ public class UserGeneratorUnitTests {
         String[] words = UserGenerator.orderNotesGenerator().split(" ");
         int numberOfWords = words.length;
         Assert.assertEquals(5, numberOfWords);
+    }
+
+    @Test
+    public void isPasswordLength10() {
+        Assert.assertEquals(10, UserGenerator.passwordGenerator().length());
     }
 }
